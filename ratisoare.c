cc7a5bcbe68507d39300cd10e7f98613a0dabac2
Alex Careja <alexandrucareja@yahoo.com>
2020-04-22 17:00:09 -0400

    Add files via upload
    25/25 ratisoare
    TODO : add comments

--- ratisoare.c
+++ ratisoare.c
@@ -14,34 +14,34 @@ typedef struct solution {
 	int time;
 } solution;
 
-typedef struct ListNode{
+typedef struct ListNode {
 	duck *elem;
 	struct ListNode *next;
 	struct ListNode *prev;
 }ListNode;  // node data type
 
-typedef struct SortedList{
+typedef struct SortedList {
 	ListNode *front;
 	ListNode *rear;
 	int size;
 }SortedList;  // sorted list data type
 
-SortedList* createList(){  // create empty list
+SortedList* createList() {  // create empty list
 	SortedList *q = (SortedList *) malloc(sizeof(SortedList));
 	q->front = NULL;
 	q->rear = NULL;
-	q->size = 0; 
+	q->size = 0;
 	return q;
-} 
+}
 
-int isListEmpty(SortedList *q){  // check if list is empty
+int isListEmpty(SortedList *q) {  // check if list is empty
 	if (q->size == 0) {
 		return 1;
 	}
 	return 0;
 }
 
-void insert(SortedList *q, duck *elem){
+void insert(SortedList *q, duck *elem) {
 	ListNode *node = (ListNode *) malloc(sizeof(ListNode));
 	node->elem = elem;
 	node->next = NULL;
@@ -84,7 +84,7 @@ void insert(SortedList *q, duck *elem){
 	q->rear = node;
 }
 
-ListNode *front(SortedList *q){  // peek front of the list
+ListNode *front(SortedList *q) {  // peek front of the list
 	return q->front;
 }
 
@@ -92,7 +92,8 @@ ListNode *rear(SortedList *q) {  // peek rear of the list
 	return q->rear;
 }
 
-void remove_node(SortedList* q, int rear){ // removes an element depending on rear = 1 / 0
+void remove_node(SortedList* q, int rear) {
+	// remove an element depending on rear = 1 / 0
 	if (isListEmpty(q)) {
 		return;
 	}
@@ -103,8 +104,7 @@ void remove_node(SortedList* q, int rear){ // removes an element depending on re
 		q->rear = aux->prev;
 		if (q->rear == NULL) {
 			q->front = NULL;
-		}
-		else {
+		} else {
 			q->rear->next = NULL;
 		}
 		free(aux);
@@ -122,7 +122,7 @@ void remove_node(SortedList* q, int rear){ // removes an element depending on re
 	free(aux);
 }
 
-void solve(solution *, SortedList *, int *, int, int);
+void solve(solution *, SortedList *, int *, int);
 void merge(solution *v, int i, int m, int j);
 void merge_sort(solution *v, int i, int j);
 
@@ -142,8 +142,7 @@ int main() {
 		d->pos = col;
 		if (direction == 'D') {
 			d->dir = 1;
-		}
-		else {
+		} else {
 			d->dir = -1;
 		}
 		insert(l[line - 1], d);
@@ -152,16 +151,11 @@ int main() {
 	int index = 0;
 	solution *s = (solution *) malloc(p * sizeof(solution));
 	for (i = 0; i < n; i++) {
-		printf("LINIA %d\n", i);
-		solve(s, l[i], &index, m, 0);
-		printf("urmatoarea linie\n");
+		solve(s, l[i], &index, m);
 	}
 	merge_sort(s, 0, p - 1);
 	FILE *o = fopen("ratisoare.out", "w");
 	fprintf(o, "%d\n", s[k - 1].id);
-	for (i = 0; i < p; i++) {
-		printf("rata %d:  %d\n", s[i].id, s[i].time);
-	}
 	fclose(o);
 	for (i = 0; i < n; i++) {
 		free(l[i]);
@@ -170,101 +164,36 @@ int main() {
 	return 0;
 }
 
-void solve(solution *s, SortedList *l, int *i, int width, int time) {
+void solve(solution *s, SortedList *l, int *i, int width) {
 	ListNode *d;
-	printf("%d = size\n", l->size);
-	int *moved = (int *) calloc(l->size, sizeof(int));
-	// take out all the ducks facing <-
-	d = front(l);
-	while (d != NULL && d->elem->dir == -1) {
-		printf("intru ca se uita in stanga\n");
-		s[*i].id = d->elem->id;
-		s[*i].time = time + d->elem->pos;
-		remove_node(l, 0);  // remove first node
-		d = front(l);
-		*i = *i + 1;
-		printf("ies\n");
-	}
-	// take out all the ducks facing ->
-	d = rear(l);
-	while (d != NULL && d->elem->dir == 1) {
-		printf("intru ca se uita in dreapta\n");
-		s[*i].id = d->elem->id;
-		s[*i].time = time + (width - d->elem->pos) + 1;
-		remove_node(l, 1);  // remove last node
-		d = rear(l);
-		*i = *i + 1;
-		printf("ies\n");
-	}
-	printf("nu da return\n");
-	if (isListEmpty(l)) {
-		return;
-	}
 	// at this point there are at least 2 ducks: -> /// <-
-	int i1, i2, i3, i4;
-	ListNode *aux1 = l->front;
-	ListNode *aux2 = aux1->next;
-	i1 = 0;
-	i2 = 1;
-	ListNode *aux3, *aux4;
-
-	//printf("inainte de while\n");
-	 // start looking for collisions
-	while (aux2 != NULL) {
-		printf("intru in while de coliziune\n");
-		if (aux1->elem->dir == 1 && aux2->elem->dir == -1) {  // ->...<-
-			if (aux2->elem->pos - aux1->elem->pos < 3){  // if there is a collision
-				if (aux2->elem->pos - aux1->elem->pos == 1) {  // -><-
-					aux1->elem->dir = -1;
-					aux2->elem->dir = 1;
-					moved[i1] = 1;
-					moved[i2] = 1;
-					// start searching backwards for collisions
-					aux3 = aux1->prev;
-					aux4 = aux1;
-					i4 = i1;
-					i3 = i1 - 1;
-					while (aux3 != NULL) {
-						if ((aux3->elem->dir == 1 && aux4->elem->dir == -1) 
-						&& (aux4->elem->pos - aux3->elem->pos == 1)) {  // -><-
-							aux3->elem->dir = -1;
-							aux4->elem->dir = 1;
-							moved[i3] = 1;
-							moved[i4] = 1;
-						} else {  // no collision backwards
-							break;
-						}
-						aux4 = aux3;
-						aux3 = aux3->prev;
-						i4 = i3;
-						i3--;
-					}
-				}
-				if (aux2->elem->pos - aux1->elem->pos == 2) { // ->_<-
-					aux1->elem->dir = -1;
-					aux2->elem->dir = 1;
-					aux1->elem->pos = aux1->elem->pos + 1;
-					aux2->elem->pos = aux2->elem->pos - 1;
-					moved[i1] = 1;
-					moved[i2] = 1;
-				}
-			}
+	int li = 0, ri = 0, j;
+	ListNode **left = (ListNode **) malloc(l->size * sizeof(ListNode *));
+	ListNode **right = (ListNode **) malloc(l->size * sizeof(ListNode *));
+	d = front(l);
+	while (d != NULL) {
+		if (d->elem->dir == -1) {
+			left[li] = d;
+			li++;
+		} else {
+			right[ri] = d;
+			ri++;
 		}
-		aux1 = aux2;
-		aux2 = aux2->next;
-		i1 = i2;
-		i2++;
+		d = d->next;
 	}
-	aux1 = l->front;
-	for (i1 = 0; aux1 != NULL; i1++, aux1 = aux1->next) {
-		if (moved[i1]) {  // the duck has collided and moved
-			continue;
-		}
-		aux1->elem->pos = aux1->elem->pos + aux1->elem->dir;
+	d = front(l); 
+	for (j = 0; j < li; j++) {
+		s[*i].id = d->elem->id;
+		s[*i].time = left[j]->elem->pos;
+		*i = *i + 1;
+		d = d->next;
 	}
-	free(moved);
-	printf("TERMIN FUNCTIA\n");
-	solve(s, l, i, width, time + 1);
+	for (j = 0; j < ri; j++) {
+		s[*i].id = d->elem->id;
+		s[*i].time = width - right[j]->elem->pos + 1;
+		*i = *i + 1;
+		d = d->next;
+	}
 }
 
 void merge(solution *v, int i, int m, int j) {
@@ -275,8 +204,7 @@ void merge(solution *v, int i, int m, int j) {
 	while (i <= m && k <= j) {
 		if (v[i].time < v[k].time) {
 			u[l++] = v[i++];
-		}
-		else {
+		} else {
 			if (v[i].time == v[k].time) {
 				if (v[i].id < v[k].id) {
 					u[l++] = v[i++];
